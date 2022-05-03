1b480d11a7ca529dadf4e738fd3147a5a8066700
stephanlanzl <stephan.lanzl@st.oth-regensburg.de>
2022-05-03 20:06:06 +0200

    added calculation of bmi in js

--- js/bmiCalculator.js
+++ js/bmiCalculator.js
@@ -1,3 +1,34 @@
 function initBMICalculator() {
     alert("Let's start coding...");
+    setInputHeightListener();
 }
+
+function setInputHeightListener(){
+    var logKey = document.getElementById("input_field_height");
+    logKey.addEventListener("input", outputBMI);
+}
+
+function outputBMI(event){
+    var height = getHeight();
+    var weight = getWeight();
+    if(height == 0){
+        console.log("Durch NULL teilen geht nicht!");
+    } else {
+        var result = weight / ((height/100) * (height/100));
+        var resInputField = document.getElementById("bmi_result_value");
+        resInputField.innerHTML = result;
+    }
+}
+
+function getHeight(){
+    var heightInputField = document.getElementById("input_field_height");
+    var height = heightInputField.value;
+    return height;
+}
+
+function getWeight(){
+    var weightInputField = document.getElementById("input_field_weight");
+    var weight = weightInputField.value;
+    return weight;
+}
+
